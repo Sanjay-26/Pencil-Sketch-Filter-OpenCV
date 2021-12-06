aa368ee2e13839aadfaa056be78b21d58757e839
Sanjay R <90435317+SanjayR-26@users.noreply.github.com>
2021-12-06 22:50:19 +0530

    Add files via upload

---
+++ pencilSketch.py
@@ -0,0 +1,23 @@
+# import the opencv library
+import cv2
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+while True:
+
+    ret, image = vid.read()
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    inverted_image = 255 - gray_image
+    blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+    inverted_blurred = 255 - blurred
+    pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=200.0)
+    cv2.imshow("Sketch", pencil_sketch)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
